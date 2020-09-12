b220127c90c35aac55d75ca53e078ec3dc49eecf
Amir Hesham <amirhesham65@gmail.com>
2020-09-12 12:23:23 +0200

    Add job data model

---
+++ src/api/models/job.ts
@@ -0,0 +1,13 @@
+class Job {
+  constructor(
+    public id: string,
+    public title: string,
+    public description: string,
+    public url: string,
+    public location: string,
+    public company: string,
+    public company_url: string,
+    public company_logo: string,
+    public created_at: string,
+  ) {}
+}
