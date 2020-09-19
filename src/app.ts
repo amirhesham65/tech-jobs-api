53f806a577145ca32ca33b690090aded2f485f57
Amir Hesham <amirhesham65@gmail.com>
2020-09-19 10:03:31 +0200

    Add default route

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import morgan from 'morgan';
 import * as dotenv from 'dotenv';
 import ExpressHttpException from "./api/utils/error";
@@ -16,6 +16,23 @@ import jobsRoutes from './api/routes/jobs';
 
 // Plugging the routes into the API
 app.use('/jobs', jobsRoutes);
+
+// The default route
+app.use('/', (req: Request, res: Response) => {
+  const rootURL: string = req.protocol + '://' + req.get('host');
+  console.log(req.protocol + '://' + req.get('host'));
+  res.json({
+    status: 200,
+    message: "Welcome to TechJobs API. An API that allows you to search and view the latest 20 tech-related jobs fetched from different sources.",
+    endpoints: {
+      search: {
+        url: "/jobs/KEYWORD",
+        usage: "Search for the latest 20 jobs related to the provided keyword",
+        example: rootURL + "/jobs/frontend"
+      }
+    }
+  });
+});
 
 // Error handling
 app.use(ExpressHttpException.expressHandleNotFoundError);
